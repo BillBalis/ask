0ca77bc322a233cf1474f1b5f18b8805f567f962
BillBalis <122732485+BillBalis@users.noreply.github.com>
2023-01-15 18:24:42 +0200

    Add files via upload

---
+++ askwww/customization.js
@@ -0,0 +1,216 @@
+
+var a;
+var b;
+
+function photo(){
+    
+    if(a==1){
+
+        document.getElementById("image1").style.display="inline";
+        return a=0;
+    }
+    else{
+
+        document.getElementById("image1").style.display="none";
+        return a=1;
+    }
+
+}
+
+function photo2(){
+    
+    if(b==1){
+
+        document.getElementById("image2").style.display="inline";
+        return b=0;
+    }
+    else{
+
+        document.getElementById("image2").style.display="none";
+        return b=1;
+    }
+
+}
+
+var c
+function story(){
+
+    if(c==1){
+
+        document.getElementById("story").style.display="inline";
+        return c=0;
+    }
+    else{
+
+        document.getElementById("story").style.display="none";
+        return c=1;
+    }
+
+}
+
+var d
+function life(){
+
+    if(d==1){
+
+        document.getElementById("life").style.display="inline";
+        return d=0;
+    }
+    else{
+
+        document.getElementById("life").style.display="none";
+        return d=1;
+    }
+
+}
+var e
+function tragedy(){
+
+    if(e==1){
+
+        document.getElementById("tragedy").style.display="inline";
+        return e=0;
+    }
+    else{
+
+        document.getElementById("tragedy").style.display="none";
+        return e=1;
+    }
+
+}
+var f
+function novel(){
+
+    if(f==1){
+
+        document.getElementById("novel").style.display="inline";
+        return f=0;
+    }
+    else{
+
+        document.getElementById("novel").style.display="none";
+        return f=1;
+    }
+
+}
+
+var g
+function link1(){
+
+    if(g==1){
+
+        document.getElementById("link1").style.display="inline";
+        return g=0;
+    }
+    else{
+
+        document.getElementById("link1").style.display="none";
+        return g=1;
+    }
+
+}
+var h
+function link2(){
+
+    if(h==1){
+
+        document.getElementById("link2").style.display="inline";
+        return h=0;
+    }
+    else{
+
+        document.getElementById("link2").style.display="none";
+        return h=1;
+    }
+
+}
+var i;
+function signin(){
+
+    if(i==1){
+
+        document.getElementById("signin").style.display="inline";
+        return i=0;
+    }
+    else{
+
+        document.getElementById("signin").style.display="none";
+        return i=1;
+    }
+
+}
+var j;
+function button1(){
+
+    if(j==1){
+
+        document.getElementById("button1").style.display="inline";
+        return j=0;
+    }
+    else{
+
+        document.getElementById("button1").style.display="none";
+        return j=1;
+    }
+
+}
+var k;
+function button2(){
+
+    if(k==1){
+
+        document.getElementById("button2").style.display="inline";
+        return k=0;
+    }
+    else{
+
+        document.getElementById("button2").style.display="none";
+        return k=1;
+    }
+
+}
+var l;
+function button3(){
+
+    if(l==1){
+
+        document.getElementById("button3").style.display="inline";
+        return l=0;
+    }
+    else{
+
+        document.getElementById("button3").style.display="none";
+        return l=1;
+    }
+
+}
+var m;
+function button4(){
+
+    if(m==1){
+
+        document.getElementById("button4").style.display="inline";
+        return m=0;
+    }
+    else{
+
+        document.getElementById("button4").style.display="none";
+        return m=1;
+    }
+
+}
+var n;
+function button5(){
+
+    if(n==1){
+
+        document.getElementById("button5").style.display="inline";
+        return n=0;
+    }
+    else{
+
+        document.getElementById("button5").style.display="none";
+        return n=1;
+    }
+
+}
